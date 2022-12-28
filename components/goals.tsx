4e410f6fe8f8b65e1715ac10a0c8e5a34468ba62
Joël Karhamba <joelkarhamba@hotmail.com>
2022-12-28 20:10:34 +0100

    goals number :zap:

--- components/goals.tsx
+++ components/goals.tsx
@@ -1,17 +1,43 @@
 import React from 'react'
 
 function Goals() {
-  return (
+    const data = [
+        {
+            id:1,
+            number:1,
+            title:'Schedule a call',
+            description: 'In dui magna, posuere eget, vestibulum et, tempor auctor, justo. Nullam dictum felis eu.',
+        },
+        {
+            id:2,
+            number:2,
+            title:'Set your goals',
+            description: 'In dui magna, posuere eget, vestibulum et, tempor auctor, justo. Nullam dictum felis eu.',
+        },
+        {
+            id:3,
+            number:3,
+            title:'Heal',
+            description: 'In dui magna, posuere eget, vestibulum et, tempor auctor, justo. Nullam dictum felis eu.',
+        },
+    ]
+        
+    return (
     <>
-    <section className='my-16 max-w-7xl	flex justify-center xl:justify-start'>
-        <article className='text-center goals'>
-            <span className='cirkel'>1</span>
-            <h2 className='my-8 pt-4'>Schedule a call</h2>
-            <p>In dui magna, posuere eget, vestibulum et, tempor auctor, justo. Nullam dictum felis eu.</p>
-        </article>
-    </section>
+        <section className='my-16 max-w-7xl	flex justify-center xl:flex-row xl:justify-start flex-col'>
+            {data.length > 0 ? data.map(({id,number,title,description})=>(
+            <>
+                <article key={id} className='text-center goals m-auto my-10 '>
+                <span className='cirkel'>{number}</span>
+                <h2 className='my-8 pt-4'>{title}</h2>
+                <p>{description}</p>
+                </article>
+            </>
+            )):'nothing'}
+            
+        </section>
     </>
 )
-}
+    }
 
 export default Goals
